ba1a26d7401f29955ebe03f42bb2e356af5521dd
bodog <bodog@DESKTOP-39D644H.home>
2018-10-12 13:30:57 +0200

    abc

---
+++ EAI_Shipping/src/main/java/com/shipping/EaiShippingApplication.java
@@ -0,0 +1,12 @@
+package com.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiShippingApplication.class, args);
+	}
+}
